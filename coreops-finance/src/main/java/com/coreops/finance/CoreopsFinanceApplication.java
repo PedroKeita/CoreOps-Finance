76a2a1c3044f2ab34e07ea7278de70f3111d5004
PedroKeita <pedrolucas300805@gmail.com>
2026-04-21 17:32:59 -0300

    chore: initialize setup project (spring + Vaadin)

---
+++ coreops-finance/src/main/java/com/coreops/finance/CoreopsFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.coreops.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreopsFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreopsFinanceApplication.class, args);
+	}
+
+}
---
+++ coreops-finance/src/test/java/com/coreops/coreops_finance/CoreopsFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coreops.coreops_finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreopsFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
